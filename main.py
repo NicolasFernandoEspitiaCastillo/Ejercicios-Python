d27ee6ad2ec1a5d5ec60e2ffb8a8f8fa64ce7947
NicolasFernandoEspitiaCastillo <186082646+NicolasFernandoEspitiaCastillo@users.noreply.github.com>
2024-11-07 13:37:23 -0500

    Ejercicio 3

--- main.py
+++ main.py
@@ -1,13 +1,42 @@
-#Ejercicio 2: Calificación de una nota
+#Ejercicio 3: Calculadora básica
 
-#Escribe un programa que determine si una nota numérica es "Aprobado" o "Reprobado" usando if.
+#Utiliza match para implementar una calculadora simple.
 
 #Enunciado:
-#Solicita al usuario una calificación y determina si la nota es aprobatoria (>= 60) o reprobatoria (<60).
+#Crea una calculadora que solicite dos números y una operación matemática (+, -, *, /). Usa match
+#para realizar la operación correspondiente.
+
+def calculadora():
+      
+      #ingrese los numeros:
+      num1 =float(input("Ingrese el primer numero: "))
+      num2 =float(input("Ingrese el segundo numero: "))
+      
+      #operacion que desea realizar:
+      Operacion = input("Ingrese la operacion a realizar (+, -, *, /): ")
+      
+      match Operacion:
+          
+          case "+":
+            resultado = num1 + num2
+            print(f"{num1} + {num2} = {resultado}")
+          case "-":
+            resultado = num1 - num2 
+            print(f"{num1} - {num2} = {resultado}") 
+          case "*":
+            print(f"{num1} * {num2} = {resultado}")
+          case "/":
+            if num2 == 0:
+               print("Error: no se puede divir por cero")
+               return
+            else: 
+               print(f"{num1} / {num2} = {resultado}")
+          case _:
+            print("Operación no válida. Por favor, elige una operación entre +, -, *, /.")
+            return  # Salir de la función si la operación no es válida
 
 
-Nota = int(input("Ingrese la nota: "))
-if Nota >=60:
-    print(f"Aprobado")
-else: 
-    print(f"Reprobado")
+calculadora()
+
+
+
